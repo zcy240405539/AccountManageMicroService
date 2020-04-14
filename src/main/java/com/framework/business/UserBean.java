205792e44bc49d0c3f560e7cf6444672ad249a24
zcy24 <zcy24@DESKTOP-FPLHI0O>
2020-04-14 17:41:39 -0500

    UserBean Update

--- src/main/java/com/framework/business/UserBean.java
+++ src/main/java/com/framework/business/UserBean.java
@@ -13,12 +13,41 @@ public class UserBean {
 	public UserRepository userRepository;
 	public UsersEntity user;
 
+	public String Checkuser(String userid) {
+		try {
+			List<UsersEntity> users = userRepository.findByUid(userid);
+			if (users.size() > 0) {
+				return "success";
+			} else
+				return "error";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "badrequest400";
+		}
+	}
+	
+	public String Checkpwd(String userid) {
+		try {
+			List<UsersEntity> users = userRepository.findByUid(userid);
+			if (users.size() > 0) {
+				user = users.get(0);
+				String pwd = user.getPwd();
+				return pwd;
+			} else
+				return "error";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "badrequest400";
+		}
+	}
+	
 	public String Auth(String userid, String pwd) {
 		
 		try {
 			List<UsersEntity> users = userRepository.findByUidAndPwd(userid, pwd);
 			if (users.size() > 0) {
-				//System.out.println(userid + " " + pwd);
 				user = users.get(0);
 				return "success";
 			} else
@@ -35,6 +64,12 @@ public class UserBean {
 		try {
 			List<UsersEntity> users = userRepository.findByUid(uid);
 			if (users.size() == 0) {
+				if(fname.equals("null"))
+					fname="";
+				if(lname.equals("null"))
+					lname="";
+				if(address.equals("null"))
+					address="";
 				UsersEntity usersEntity = new UsersEntity(uid, pwd, fname,lname, address);
 				userRepository.save(usersEntity);
 				return "success";
@@ -50,7 +85,7 @@ public class UserBean {
 	public String Destroy(String uid, String pwd) {
 		try {
 			List<UsersEntity> users = userRepository.findByUidAndPwd(uid, pwd);
-			if ((users.size() > 0) && (Auth(uid,pwd)=="success")) {
+			if ((users.size() > 0) && (Auth(uid,pwd).equals("success"))) {
 				for(UsersEntity usersEntity:users) {
 					userRepository.delete(usersEntity);	
 				}	
@@ -64,27 +99,31 @@ public class UserBean {
 		}
 	}
 
-	public String Update(String uid,String currentpwd, String pwd, String fname, String address) {
+	public String Update(String uid, String pwd, String fname, String lname, String address) {
 		try {
 			List<UsersEntity> users = userRepository.findByUid(uid);
-			//UsersEntity newUsersEntity = new UsersEntity(uid, pwd, fname, address);
-			if ((users.size() > 0) && (Auth(uid,currentpwd)=="success")) {
+
+			if (users.size() > 0 ) {
 				// set password
 				if(pwd!=""&&pwd!=null)
 					users.get(0).setPwd(pwd);
 				else
 					users.get(0).setPwd(users.get(0).getPwd());
-				// set user name
+				// set user first name
 				if(fname!=""&&fname!=null)
 					users.get(0).setFirstName(fname);
 				else
 					users.get(0).setFirstName(users.get(0).getFirstName());
+				// set user last name
+				if(fname!=""&&fname!=null)
+					users.get(0).setLastName(lname);
+				else
+					users.get(0).setLastName(users.get(0).getLastName());			
 				//set address
 				if(address!=""&&address!=null)
 					users.get(0).setAddress(address);
 				else
 					users.get(0).setAddress(users.get(0).getAddress());
-				
 				// save
 				userRepository.save(users.get(0));
 				return "success";
--- src/main/java/com/framework/controller/UserRestMicroService.java
+++ src/main/java/com/framework/controller/UserRestMicroService.java
@@ -2,6 +2,9 @@ package com.framework.controller;
 
 import com.framework.business.*;
 import com.framework.jpa.*;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +13,32 @@ public class UserRestMicroService {
 	@Autowired
 	UserBean userBean;
 	
+	
+	@GetMapping(value="/checkuser/{uid}")
+	public String Checkuser(@PathVariable String uid) {
+		String result = userBean.Checkuser(uid);
+		if (result.equals("success")) {
+			return "success";
+		} else {
+			return "error";
+		}
+		
+	}
+		
+	@GetMapping(value="/checkpwd/{uid}")
+	public String Checkpwd(@PathVariable String uid) {
+		String pwd = userBean.Checkpwd(uid);
+		if(pwd.equals("error")) {
+			return "error";
+		}else {
+			return pwd;
+		}
+	}
+	
 	@GetMapping(value="/authRest/{uid}/{pwd}")
 	public String login(@PathVariable String uid, @PathVariable String pwd) {
 		String ue = userBean.Auth(uid, pwd);
-		if(ue == "success") {
+		if(ue.equals("success")) {
 			//System.out.println("success");
 			return "success";
 		}else {
@@ -25,7 +50,7 @@ public class UserRestMicroService {
 	@GetMapping(value="/registRest/{uid}/{pwd}/{fname}/{lname}/{address}")
 	public String register(@PathVariable String uid, @PathVariable String pwd, @PathVariable String fname, @PathVariable String lname, @PathVariable String address) {
 		String ue = userBean.Register(uid, pwd, fname, lname, address);
-		if(ue == "success") {
+		if(ue.equals("success")) {
 			//System.out.println("success");
 			return "success";
 		}else {
@@ -37,7 +62,7 @@ public class UserRestMicroService {
 	@GetMapping(value="/destroyRest/{uid}/{pwd}")
 	public String destroy(@PathVariable String uid, @PathVariable String pwd) {
 		String ue = userBean.Destroy(uid, pwd);
-		if(ue == "success") {
+		if(ue.equals("success")) {
 			//System.out.println("success");
 			return "success";
 		}else {
@@ -49,7 +74,7 @@ public class UserRestMicroService {
 	@GetMapping(value="/updateRest/{uid}/{pwd}/{fname}/{lname}/{address}")
 	public String update(@PathVariable String uid, @PathVariable String pwd, @PathVariable String fname, @PathVariable String lname, @PathVariable String address) {
 		String ue = userBean.Update(uid, pwd, fname, lname, address);
-		if(ue == "success") {
+		if(ue.equals("success")) {
 			//System.out.println("success");
 			return "success";
 		}else {
